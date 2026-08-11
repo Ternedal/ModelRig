61d6fc63728b555f1e77fd101cca91276c37f17f
Ternedal <a.nielbaek@gmail.com>
2026-08-11 14:11:34 +0200

    test(android): use JUnit for audit presentation tests

--- android/app/src/test/java/dk/ternedal/modelrig/ui/ControlCenterAuditSectionTest.kt
+++ android/app/src/test/java/dk/ternedal/modelrig/ui/ControlCenterAuditSectionTest.kt
@@ -1,7 +1,7 @@
 package dk.ternedal.modelrig.ui
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class ControlCenterAuditSectionTest {
     @Test
