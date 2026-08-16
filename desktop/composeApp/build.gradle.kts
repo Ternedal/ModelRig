7dd693fb1bc87f8e27946db4abc9c94052c89ebb
Ternedal <a.nielbaek@gmail.com>
2026-08-16 11:54:42 +0200

    Re-cut physical proof 2.0.9 from main 377c137

--- desktop/composeApp/build.gradle.kts
+++ desktop/composeApp/build.gradle.kts
@@ -47,7 +47,7 @@ compose.desktop {
             // lets packageVersion tell the truth.
             targetFormats(TargetFormat.Deb)
             packageName = "Kaliv"
-            packageVersion = "2.0.8"
+            packageVersion = "2.0.9"
         }
     }
 }
